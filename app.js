fea1fc9750822c65080b6cf4c490b5874535ac20
marceaudavid <marceau.david@ynov.com>
2021-11-18 15:12:33 +0100

    finish microservice

--- app.js
+++ app.js
@@ -1,54 +1,134 @@
+const express = require('express');
+const axios = require('axios');
+const bodyparser = require('body-parser');
+const crypto = require('crypto');
 
-const express = require('express')
-const axios = require('axios')
-const bodyparser = require('body-parser')
-const crypto = require('crypto')
-
-const app = express()
-app.use(bodyparser.json())
+const app = express();
+app.use(bodyparser.json());
 const port = 3000;
 
+let keys;
 
 // Fonction lancée eu démarrage du service
 const start = async () => {
+  // Envoie d'une requete
+  let response;
+  try {
+    response = await axios.post(
+      'http://10.44.17.33:1338/register',
+      { host: 'http://10.44.16.131:3000', code: 'marceau.david' },
+      {
+        auth: {
+          username: 'ynovset',
+          password: 'tHuds4752_525@',
+        },
+      }
+    );
+  } catch (e) {
+    console.error(e.response ? e.response.data : e);
+    return;
+  }
+  keys = response.data;
+  try {
+    response = await axios.get('http://10.44.17.33:1338/registry', {
+      headers: {
+        'X-Auth-Token': keys.token,
+      },
+    });
+  } catch (e) {
+    console.error(e.response ? e.response.data : e);
+    return;
+  }
+  await loop(response.data);
+};
 
-    // Envoie d'une requete
-    let response;
+const unlock = async (host, code) => {
+  let response, key;
+  try {
+    response = await axios.get(`${host}/getkey`, {
+      headers: { 'X-Auth-Token': keys.token },
+    });
+  } catch (e) {
+    console.error(
+      '\x1b[31m',
+      `${code}: cannot get key ${e.response ? `(${e.response.status})` : ''}`
+    );
+    return `${code}: cannot get key`;
+  }
+  if (response && response.data && response.data.encrypted_public_key) {
+    key = response.data.encrypted_public_key;
     try {
-        response = await axios.post("http://<host>:<port>", {"data_key" : 'hello'}, {headers : { 'my-custom-header' : 'xxx'}});
-    }catch(e){
-        console.error(e.response ? e.response.data : e)
-        return
+      response = await axios.post(
+        'http://10.44.17.33:1338/key/unlock',
+        { code, key },
+        { headers: { 'X-Auth-Token': keys.token } }
+      );
+    } catch (e) {
+      console.error('\x1b[31m', `${code}: cannot unlock key`);
+      return `${code}: cannot get key`;
     }
-    console.log(response.data);
+    console.log('\x1b[32m', `${code}: ${host}`);
+    return `${code}: ${host}`;
+  }
+};
 
-}
+const loop = async (registry) => {
+  await Promise.all(
+    registry.map(async ({ code, host }) => {
+      await unlock(host, code);
+    })
+  );
+};
+
+app.get('/ping', (req, res) => {
+  res.status(200).send('OK');
+});
 
 // Création d'un endpoint en GET
-app.get('/url_du_endpoint_en_get', async (req, res) => {
-    // Récuperer les headers
-    let headers = req.headers;
-    res.json()
-})
+app.get('/getkey', async (req, res) => {
+  // Récuperer les headers
+  let token = req.headers['x-auth-token'];
+  if (!token) {
+    res.status(403).send('Authentification invalide');
+    return;
+  }
+  let response;
+  try {
+    response = await axios.post(
+      'http://10.44.17.33:1338/token/validate',
+      { token },
+      {
+        headers: {
+          'X-Auth-Token': keys.token,
+        },
+      }
+    );
+  } catch (error) {
+    if (error.response.status === 500) {
+      res.status(502).send('L’annuaire est indisponible, impossible de vérifier le jeton');
+      return;
+    }
+    res.status(500).send('Erreur inattendue');
+    return;
+  }
+  if (!response.data.valid) {
+    res.status(403).send('Authentification invalide');
+    return;
+  }
+  let encrypted_public_key = encrypt(keys.secret_key, keys.public_key);
+  res.status(200).json({ encrypted_public_key });
+});
 
-// Création d'un endpoint en POST
-app.post('/url_du_endpoint_en_post', async (req, res) => {
-    // Récuération du body
-    let body = req.body;
-    let headers = req.headers;
-    console.log(body)
-    console.log(headers)
-    res.json()
-})
-
+app.post('/newservice', async (req, res) => {
+  let { host, code } = req.body;
+  await unlock(host, code);
+});
 
 // Lancement du service
 app.listen(port, () => {
-    console.log(`Service listening at http://localhost:${port}`)
-    start();
-})
-
-
+  console.log('\x1b[32m', `Service listening at http://localhost:${port}`);
+  start();
+});
 
 /**
  * Fonction de chiffrement
@@ -56,8 +136,6 @@ app.listen(port, () => {
  * @param publicKey
  * @returns {string}
  */
-function encrypt(secretKey , publicKey){
-    return crypto.createHmac('sha256', secretKey)
-        .update(publicKey)
-        .digest('hex');
+function encrypt(secretKey, publicKey) {
+  return crypto.createHmac('sha256', secretKey).update(publicKey).digest('hex');
 }
